668d88675483e28fcc1057ec4f9a79814004abdb
astrezh <maurinaag@mail.ru>
2020-10-19 00:43:55 +0300

    Create dz0.php

---
+++ dz0.php
@@ -0,0 +1,20 @@
+
+<?php
+
+/*Приведите этот код к соответствию стандарту PSR-2*/
+
+const TEST_CONSTANTA = 'asd';
+
+$user_name = "Igor";
+
+function show_something()
+{
+	return 'SOMETHING';
+
+}
+
+if (1==1) {
+	
+	echo 'hi';
+	
+}
