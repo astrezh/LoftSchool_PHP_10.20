cebbfd3e01b4d8935591412c189f092a3b115498
astrezh <maurinaag@mail.ru>
2020-10-21 01:19:16 +0300

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -1,1 +1,38 @@
 
+/**
+Задание #3 (Использование рекурсии не обязательно)
+
+    Функция должна принимать два параметра – целые числа.
+    Если в функцию передали 2 целых числа, то функция должна отобразить таблицу умножения размером со значения параметров, переданных в функцию. (Например если передано 8 и 8, то нарисовать от 1х1 до 8х8). Таблица должна быть выполнена с использованием тега <table>
+    В остальных случаях выдавать корректную ошибку.
+ */
+function task3($a, $b)
+{
+    if (!is_int($a)) {
+        trigger_error('A is not integer');
+        return false;
+    }
+    if (!is_int($b)) {
+        trigger_error('B is not integer');
+        return false;
+    }
+
+    if ($a < 0 || $b < 0) {
+        trigger_error('Argumentsmust be positive');
+        return false;
+    }
+
+    $result = '<table>';
+    for ($i = 1; $i <= $a; $i++) {
+        $result .= '<tr>';
+        for ($j = 1; $j <= $b; $j++) {
+            $result .= '<td>';
+            $result .= $i * $j;
+            $result .= '</td>';
+        }
+        $result .= '</tr>';
+    }
+    $result .= '</table>';
+    echo $result;
+}
+task3(2, 2);
